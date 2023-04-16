67d91f3162588522c0f82d507070fbcaa87c7d59
YASHSHARMA111 <yash.sharma9295@gmail.com>
2023-04-17 00:29:24 +0530

    first commit

---
+++ loginregreactjsmui1/src/pages/Contact.js
@@ -0,0 +1,16 @@
+import { Grid } from "@mui/material";
+
+const Contact = () => {
+  return <>
+    <Grid container justifyContent='center'>
+      <Grid item sm={10}>
+        <h1>About Page</h1>
+        <hr />
+        <p>We are a community of friendly, open-minded, collaborative and professionally growing people.
+Our team is made up of some of the brightest minds in statistics, math, programming, and technology and we are obsessed with how AI is defining the customer experience.</p>
+      </Grid>
+    </Grid>
+  </>;
+};
+
+export default Contact;
---
+++ loginregreactjsmui1/src/pages/Home.js
@@ -0,0 +1,15 @@
+import { Grid } from "@mui/material";
+
+const Home = () => {
+  return <>
+    <Grid container justifyContent='center'>
+      <Grid item sm={10}>
+        <h1>Home Page</h1>
+        <hr />
+        <p>Hire pre-vetted, in-house, remote AI consultants. No long-term commitments. Services include POC development, MVP, and complete AI solutions.</p>
+        </Grid>
+    </Grid>
+  </>;
+};
+
+export default Home;
